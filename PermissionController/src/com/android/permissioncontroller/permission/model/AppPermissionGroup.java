2f91142b245523d47ca18610172bca81df7200c8
Daniel Micay <danielmicay@gmail.com>
2017-07-22 21:43:50 -0400

    always treat INTERNET as a runtime permission
    ported from 10: 6c4f112dde47f21ce5a583f5bd8b217db6de5c02
    
    Change-Id: I3ff9d3176930ecb42b04a0e591ec6d176e1f26ff

--- PermissionController/src/com/android/permissioncontroller/permission/model/AppPermissionGroup.java
+++ PermissionController/src/com/android/permissioncontroller/permission/model/AppPermissionGroup.java
@@ -36,6 +36,7 @@ import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.PermissionGroupInfo;
 import android.content.pm.PermissionInfo;
+import android.Manifest;
 import android.os.Binder;
 import android.os.Build;
 import android.os.UserHandle;
@@ -925,7 +926,7 @@ public final class AppPermissionGroup implements Comparable<AppPermissionGroup> 
 
             boolean wasGranted = permission.isGrantedIncludingAppOp();
 
-            if (mAppSupportsRuntimePermissions) {
+            if (mAppSupportsRuntimePermissions || Manifest.permission.INTERNET.equals(permission.getName())) {
                 // Do not touch permissions fixed by the system.
                 if (permission.isSystemFixed()) {
                     wasAllGranted = false;
@@ -1113,7 +1114,7 @@ public final class AppPermissionGroup implements Comparable<AppPermissionGroup> 
                 break;
             }
 
-            if (mAppSupportsRuntimePermissions) {
+            if (mAppSupportsRuntimePermissions || Manifest.permission.INTERNET.equals(permission.getName())) {
                 // Revoke the permission if needed.
                 if (permission.isGranted()) {
                     permission.setGranted(false);
